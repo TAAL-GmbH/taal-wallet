8e5f765be2520ecb48c524a756f8c3fff2eca6dd
Arnaldas Augutis <arnaldas@taal.com>
2022-08-05 13:47:31 +0300

    BPAAS-0000 manifest.json cleanup

--- src/manifest.ts
+++ src/manifest.ts
@@ -3,42 +3,25 @@ import { ManifestType } from '@/src/manifest-type';
 
 const manifest: ManifestType = {
   manifest_version: 3,
-  // name: packageJson.name,
   name: 'TAAL Wallet',
   version: packageJson.version,
   description: packageJson.description,
   options_page: 'src/pages/options/index.html',
   background: { service_worker: 'src/pages/background/index.js' },
-  permissions: ['storage', 'notifications', 'alarms'],
-  host_permissions: ['*://localhost/*'],
+  permissions: ['notifications', 'alarms'],
   externally_connectable: {
-    matches: ['*://localhost/*'],
+    matches: ['http://localhost/*', 'https://*.taal.com/*'],
   },
   action: {
     default_popup: 'src/pages/popup/index.html',
     default_icon: 'taal-round-34x34.png',
   },
-  // chrome_url_overrides: {
-  //   newtab: 'src/pages/newtab/index.html',
-  // },
   icons: {
     '128': 'taal-round-128x128.png',
   },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['src/pages/content/index.js'],
-      // css: ['contentStyle.css'],
-    },
-  ],
-  // devtools_page: 'src/pages/devtools/index.html',
   web_accessible_resources: [
     {
-      resources: [
-        // 'contentStyle.css',
-        'taal-round-128x128.png',
-        'taal-round-34x34.png',
-      ],
+      resources: ['taal-round-128x128.png', 'taal-round-34x34.png'],
       matches: [],
     },
   ],
--- src/pages/auth/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createPage } from '@/src/utils/createPage';
-import { Auth } from '@/pages/auth/Auth';
-
-createPage({
-  domElement: document.querySelector('#app-container'),
-  component: <Auth />,
-});
