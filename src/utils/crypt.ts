35f8ee1a687ca3d32721da1cd5a7c51ba6f41b97
Arnaldas Augutis <arnaldas@taal.com>
2024-03-21 11:56:18 +0200

    removed window dependency from crypto.ts

--- src/utils/crypt.ts
+++ src/utils/crypt.ts
@@ -2,15 +2,15 @@ const iv = Buffer.from('f98c2b14c2d893e7ae9bdf1da4170fc3', 'hex');
 
 export const sha256 = async (str: string) => {
   const buffer = new TextEncoder().encode(str);
-  return window.crypto.subtle.digest('SHA-256', buffer);
+  return crypto.subtle.digest('SHA-256', buffer);
 };
 
 export const encrypt = async (str: string, password: string) => {
   const hash = await sha256(password);
   const key = new Uint8Array(hash.slice(0, 32)); // Truncate to 256 bits (32 bytes)
   const encodedStr = new TextEncoder().encode(str);
-  const keyBuffer = await window.crypto.subtle.importKey('raw', key, 'AES-CBC', false, ['encrypt']);
-  const encryptedBuffer = await window.crypto.subtle.encrypt({ name: 'AES-CBC', iv }, keyBuffer, encodedStr);
+  const keyBuffer = await crypto.subtle.importKey('raw', key, 'AES-CBC', false, ['encrypt']);
+  const encryptedBuffer = await crypto.subtle.encrypt({ name: 'AES-CBC', iv }, keyBuffer, encodedStr);
   return Array.from(new Uint8Array(encryptedBuffer))
     .map(b => b.toString(16).padStart(2, '0'))
     .join('');
@@ -20,11 +20,7 @@ export const decrypt = async (encryptedHex: string, password: string) => {
   const hash = await sha256(password);
   const key = new Uint8Array(hash.slice(0, 32)); // Truncate to 256 bits (32 bytes)
   const encryptedBytes = Uint8Array.from(Buffer.from(encryptedHex, 'hex'));
-  const keyBuffer = await window.crypto.subtle.importKey('raw', key, 'AES-CBC', false, ['decrypt']);
-  const decryptedBuffer = await window.crypto.subtle.decrypt(
-    { name: 'AES-CBC', iv },
-    keyBuffer,
-    encryptedBytes
-  );
+  const keyBuffer = await crypto.subtle.importKey('raw', key, 'AES-CBC', false, ['decrypt']);
+  const decryptedBuffer = await crypto.subtle.decrypt({ name: 'AES-CBC', iv }, keyBuffer, encryptedBytes);
   return new TextDecoder().decode(decryptedBuffer);
 };
